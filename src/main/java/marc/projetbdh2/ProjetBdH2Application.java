030828c08763de23e777e391c9133e8c6008b2d8
marc-antoine mercier <marc.mercier.mam@gmail.com>
2025-01-04 16:50:51 -0500

    initial commit

---
+++ src/main/java/marc/projetbdh2/ProjetBdH2Application.java
@@ -0,0 +1,13 @@
+package marc.projetbdh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBdH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetBdH2Application.class, args);
+    }
+
+}
---
+++ src/test/java/marc/projetbdh2/ProjetBdH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package marc.projetbdh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBdH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
